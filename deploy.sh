2119528bd9464e49bec9e2396ff60850123177d4
jinxiaodong <jinxiaodong@hikvision.com.cn>
2020-10-21 10:31:23 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "deploying"
 npm config set registry https://registry.npm.taobao.org
-npm install --no-lockfile
+npm install
 npm run build:prod
 scp -r ./dist/* /usr/share/nginx/html
 echo "deploy done"
